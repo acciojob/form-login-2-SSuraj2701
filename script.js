7cda26c01b853098f7fc48bc424c1f11cf6db79b
Suraj Singh <suraj.job.2002@gmail.com>
2025-11-17 17:08:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-//your JS code here. If re
-document.getElementById("btn").addEventListener("click",()=>{
-	let first=document.getElementById("firstname").value;
-	let second=document.getElementById("lastname").value;
-	let third=document.getElementById("phone").value;
-	let fourt=document.getElementById("email").value;
-	let a=`First Name: ${first} Last Name:${second} Phone Number: ${third} Email ID: ${fourt} `;
-     alert(a);
-})
-let a=`First Name: ${first} Last Name:${second} Phone Number: ${third} Email ID: ${fourt} `;
-alert(a);
+ //your JS code here
+document.getElementById("btn").addEventListener("click", () => {
+    let first = document.getElementById("firstname").value;
+    let second = document.getElementById("lastname").value;
+    let third = document.getElementById("phone").value;
+    let fourt = document.getElementById("email").value;
+
+    let a = `First Name: ${first} Last Name: ${second} Phone Number: ${third} Email ID: ${fourt}`;
+    alert(a);
+});
