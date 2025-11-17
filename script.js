bfc824974e5139b79dad8c55871f479f22e17582
Suraj Singh <suraj.job.2002@gmail.com>
2025-11-17 17:13:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
  //your JS code here
 document.getElementById("btn").addEventListener("click", () => {
-    let first = document.getElementById("firstname").value;
-    let second = document.getElementById("lastname").value;
-    let third = document.getElementById("phone").value;
-    let fourt = document.getElementById("email").value;
+    let first = document.getElementById("First Name").value;
+    let second = document.getElementById("Last Name").value;
+    let third = document.getElementById("Phone Number").value;
+    let fourt = document.getElementById("Email ID").value;
 
     let a = `First Name: ${first} Last Name: ${second} Phone Number: ${third} Email ID: ${fourt}`;
     alert(a);
